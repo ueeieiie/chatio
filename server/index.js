cbb2a043c5f85c39139df320c7623f20c22fe54c
ueeieiie <udielenberg@gmail.com>
2018-02-19 04:53:34 +0200

    separated socket concerns to different module

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const socket = require('socket.io');
 const path = require('path');
-const uuid = require('uuid/v4');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -15,47 +14,4 @@ const server = app.listen(PORT, () => {
 
 // Socket setup
 const io = socket(server);
-const connections = [];
-const users = [];
-const messages = [];
-
-
-io.on('connection', (socket) => {
-	connections.push(socket);
-	console.log('Connected: %s sockets are active', connections.length);
-
-	socket.on('REGISTER_USER', (data) => {
-		const newUser = createNewUser(data);
-		socket.emit('RETURN_USER_INFO', newUser);
-	});
-
-
-	socket.on('SEND_MESSAGE', (data) => {
-		const { userId, avatar, username } = data.user;
-		const { message } = data;
-		io.emit("RECEIVE_MESSAGE", {userId, username, message, avatar,});
-	})
-
-	socket.on('disconnect', (data) => {
-		connections.splice(connections.indexOf('socket'), 1);
-		console.log('Disconnected: %s sockets are active', connections.length);
-	})
-});
-
-const createNewUser = username => {
-	const userId = createUserId();
-	const avatar = createAvatar(username, userId);
-
-	users.push({userId, username, avatar});
-	
-	return {userId, username, avatar};
-}
-
-const createUserId = () => {
-	const userId = uuid();
-	return userId;
-}
-
-const createAvatar = (username, id) => {
-	return `https://robohash.org/${username}${id}.png`;
-}
+require('./socket')(io);
---
+++ server/socket.js
@@ -0,0 +1,44 @@
+// Dependencies
+const uuid = require('uuid/v4');
+
+const connections = [];
+const users = [];
+const messages = [];
+
+module.exports = (io) => {
+	io.on('connection', (socket) => {
+		connections.push(socket);
+		console.log('Connected: %s sockets are active', connections.length);
+	
+		socket.on('REGISTER_USER', (data) => {
+			const newUser = createNewUser(data);
+			socket.emit('RETURN_USER_INFO', newUser);
+		});
+	
+		socket.on('SEND_MESSAGE', (data) => {
+			const { userId, avatar, username } = data.user;
+			const { message } = data;
+			
+			io.emit("RECEIVE_MESSAGE", {userId, username, message, avatar,});
+		})
+	
+		socket.on('disconnect', (data) => {
+			connections.splice(connections.indexOf('socket'), 1);
+			console.log('Disconnected: %s sockets are active', connections.length);
+		})
+	});
+};
+
+const createNewUser = username => {
+	const userId = uuid();
+	const avatar = createAvatar(username, userId);
+
+	users.push({userId, username, avatar});
+	
+	return {userId, username, avatar};
+}
+
+
+const createAvatar = (username, id) => {
+	return `https://robohash.org/${username}${id}.png`;
+}
